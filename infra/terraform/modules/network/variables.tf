206c29aaf80a7f9b600a94b36e49b3313e540611
Gabriel <gsmainiere1901@gmail.com>
2026-06-16 23:21:44 -0300

    feat: creating network resources

---
+++ infra/terraform/modules/network/variables.tf
@@ -0,0 +1,34 @@
+variable "project_name" {
+    description = "Nome do projeto"
+    type = string
+}
+
+variable "environment" {
+    description = "Ambiente (ex: dev)"
+    type = string
+}
+
+variable "location" {
+    description = "Localizacão dos recursos"
+    type = string
+}
+
+variable "address_space" {
+    description = "Espaços de enderecamento da rede"
+    type = list(string)
+}
+
+variable "vm_subnet_address_prefixes" {
+    description = "Lista de prefixos de enderecamento para subnets de maquinas virtuais"
+    type = list(string)
+}
+
+variable "aks_subnet_address_prefixes" {
+    description = "Lista de prefixos de enderecamento para subnets do AKS"
+    type = list(string)
+}
+
+variable "tags" {
+    description = "Tags para recursos"
+    type = map(string)
+}
